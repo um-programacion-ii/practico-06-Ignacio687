2ae104f5c665d56e56d54ece08ffe7c4e9d6f5a2
Ignacio687 <ignadavidchaves@gmail.com>
2024-05-15 14:00:53 -0300

    tests e implementacíon GestionDrogueriaService listos

--- src/main/java/ar/edu/um/programacion2/curso2024/service/DrugstoreManagementService.java
+++ src/main/java/ar/edu/um/programacion2/curso2024/service/DrugstoreManagementService.java
@@ -1,12 +1,13 @@
 package ar.edu.um.programacion2.curso2024.service;
 
 import ar.edu.um.programacion2.curso2024.entity.Medicamento;
+import ar.edu.um.programacion2.curso2024.entity.customExceptions.NotEnabledToRunException;
 import ar.edu.um.programacion2.curso2024.entity.transaccion.Pedido;
 
 import java.util.List;
 
 public interface DrugstoreManagementService {
     Pedido iniciarPedido(List<Medicamento> medicamentos);
-    void finalizarPedido(Pedido pedido);
+    void finalizarPedido(Pedido pedido) throws NotEnabledToRunException;
     List<Pedido> verPedidos();
 }
